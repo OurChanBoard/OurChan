9337f6229416f54fd45af8910723315a3dc16613
CallMeTheDrink <bigcat@acehole.exposed>
2025-04-28 12:40:42 -0400

    Fixed theme selection support for non JS users

--- app.js
+++
@@ -1,8 +0,0 @@
-const themeMiddleware = require('./middleware/theme');
-const themeRoutes = require('./routes/theme');
-
-// Add theme middleware before route handlers
-app.use(themeMiddleware);
-
-// Add theme routes
-app.use('/', themeRoutes); 
